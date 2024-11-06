066508f43d949abf4b9ac1924afbdb2ca0d65d5d
gbroer <184684191+gbroer@users.noreply.github.com>
2024-11-06 20:14:08 +0100

    Add function to get human's choice

--- script.js
+++ script.js
@@ -13,3 +13,54 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
+
+// function getHumanChoice() {
+//     let answer = prompt("Enter r for Rock, p for Paper or s for Scissors:");
+//     let humanChoice;
+//     if (answer === "r") {
+//         humanChoice = "Rock";
+//     } else if (answer === "p") {
+//         humanChoice = "Paper"
+//     } else if (answer === "s") {
+//         humanChoice = "Scissors"
+//     } else {
+//         prompt("Try again. Press r, p or s:")
+//     }
+//     console.log(answer);
+//     console.log(typeof answer);
+//     console.log(humanChoice);
+//     console.log(typeof humanChoice);
+// }
+
+// getHumanChoice();
+
+function getHumanChoice() {
+    let humanChoice;
+    while (!humanChoice) {
+        let answer = prompt("Enter r for Rock, p for Paper or s for Scissors:");
+        if (answer === "r") {
+            humanChoice = "Rock";
+            console.log(answer);
+            console.log(typeof answer);
+            console.log(humanChoice);
+            console.log(typeof humanChoice);
+            break;
+        } else if (answer === "p") {
+            humanChoice = "Paper";
+            console.log(answer);
+            console.log(typeof answer);
+            console.log(humanChoice);
+            console.log(typeof humanChoice);            
+            break;
+        } else if (answer === "s") {
+            humanChoice = "Scissors";
+            console.log(answer);
+            console.log(typeof answer);
+            console.log(humanChoice);
+            console.log(typeof humanChoice);            
+            break;
+        }
+    }
+}
+
+getHumanChoice()
