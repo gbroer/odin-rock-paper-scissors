024c77fc1520309f576797b61008ebf18f3b993a
gbroer <184684191+gbroer@users.noreply.github.com>
2024-11-17 15:31:47 +0100

    Rewrite functions to get human and computer choices

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
+// create function to get computer's choice
+// get random number between 0 and 1
+// if random number is below 1/3, computer's choice is rock
+// if random number is between 1/3 and 2/3, computer's choice is paper
+// if random number is over 2/3, computer's choice is scissors
+
 function getComputerChoice() {
     let randomNum = Math.random();
+    let computerChoice = "";
     if (randomNum <= 1/3) {
-        computerChoice = "Rock"
+        computerChoice = "Rock";
     } else if (2/3 > randomNum > 1/3) {
-        computerChoice = "Paper"
+        computerChoice = "Paper";
     } else {
-        computerChoice = "Scissors"
+        computerChoice = "Scissors";
     }
-
-    console.log(randomNum);
-    console.log(computerChoice);
 }
 
-getComputerChoice();
 
+// create function to get human's choice
+// prompt user to enter "rock", "paper" of "scissors"
+// check lowercase version of answer
+// if answer is not "rock", "paper" or "scissors", prompt again
+// if answer is "rock", "paper" or "scissors" assign corresponding value to variable humanChoice
 
 function getHumanChoice() {
-    let humanChoice;
+    let humanChoice = "";
     while (!humanChoice) {
-        let answer = prompt("Enter r for Rock, p for Paper or s for Scissors:");
-        if (answer === "r") {
+        let answer = prompt("Rock, paper or scissors?");
+        if (answer.toLowerCase() === "rock") {
             humanChoice = "Rock";
-            break;
-        } else if (answer === "p") {
+        } else if (answer.toLowerCase() === "paper") {
             humanChoice = "Paper";            
-            break;
-        } else if (answer === "s") {
+        } else if (answer.toLowerCase() === "scissors") {
             humanChoice = "Scissors";
-            break;
         }
     }
 }
 
-getHumanChoice()
+let humanScore = 0;
+let computerScore = 0;
